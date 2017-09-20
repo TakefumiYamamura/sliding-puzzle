eceae7db8a0fc2994d49f449d856bbedcaf2156c
TakefumiYamamura <yama1223xxx@gmail.com>
2017-09-20 19:29:54 +0900

    stack solver

--- src/15puzzle.cc
+++ src/15puzzle.cc
@@ -1,136 +1,145 @@
 #include <iostream>
-#include <vector>
 #include <assert.h>
-#include <map>
+#include <vector>
+ 
 #include <cmath>
 #include <algorithm>
-#include <queue>
-#include <unordered_set>
-#include <unordered_map>
 #include <string>
 #include <set>
 #include <climits>
-
+#include <stack>
+ 
+#define N 4
+#define N2 16
+ 
 using namespace std;
-
-const int N = 4;
-const int N2 = 16;
-
+ 
+// const int N = 4;
+// const int N2 = 16;
+ 
 static const int dx[4] = {0, -1, 0, 1};
 static const int dy[4] = {1, 0, -1, 0};
 static const char dir[4] = {'r', 'u', 'l', 'd'}; 
-
-
+ 
+ 
 struct Node
 {
-	vector<int> puzzle;
-	int space;
-	int md;
+    int puzzle[N2];
+    int space;
+    int md;
+    int depth;
+    int pre;
 };
-
+ 
 class Npuzzle
 {
 private:
-	Node s_node;
-	int limit;
-	vector<vector<int> > md;
-	vector<int> path;
-	int ans;
+    Node s_node;
+    Node cur_n;
+    int md[N2][N2];
+    int ans;
 public:
-	Npuzzle() {
-		s_node = Node();
-		vector<int> in;
-		for (int i = 0; i < N2; ++i)
-		{
-			int tmp;
-			cin >> tmp;
-			if(tmp == 0) {
-				tmp = N2;
-				s_node.space = i;
-			}
-			in.push_back(tmp);
-		}
-		s_node.puzzle = in;
-		set_md();
-		s_node.md = get_md_sum(s_node.puzzle);
-	}
+    Npuzzle() {
+        s_node = Node();
+        int in[N2];
+        for (int i = 0; i < N2; ++i)
+        {
+            int tmp;
+            cin >> tmp;
+            if(tmp == 0) {
+                tmp = N2;
+                s_node.space = i;
+            }
+            s_node.puzzle[i] = tmp;
+        }
+        set_md();
+        s_node.md = get_md_sum(s_node.puzzle);
+        s_node.depth = 0;
+        s_node.pre = -10;
+    }
+ 
+    int get_md_sum(int *puzzle) {
+        int sum = 0;
+        for (int i = 0; i < N2; ++i)
+        {
+            if(puzzle[i] == N2) continue;
+            sum += md[i][puzzle[i]-1];
+        }
+        return sum;
+    }
+ 
+    void set_md() {
+        for (int i = 0; i < N2; ++i)
+        {
+            for (int j = 0; j < N2; ++j)
+            {
+                md[i][j] = abs(i / N - j / N) + abs(i % N - j % N);
+            }
+        }
+    }
 
-	int get_md_sum(const vector<int>& puzzle) {
-		int sum = 0;
-		for (int i = 0; i < N2; ++i)
-		{
-			if(puzzle[i] == N2) continue;
-			sum += md[i][puzzle[i]-1];
-		}
-		return sum;
-	}
+    bool dfs(int limit) {
+        stack<Node> st;
+        st.push(s_node);
 
-	void set_md() {
-		md = vector<vector<int> >(N2, vector<int>(N2, 0));
-		for (int i = 0; i < N2; ++i)
-		{
-			for (int j = 0; j < N2; ++j)
-			{
-				md[i][j] = abs(i / N - j / N) + abs(i % N - j % N);
-			}
-		}
-	}
+        while(!st.empty()) {
+            Node cur_n = st.top();
+            st.pop();
+            if(cur_n.md == 0 ) {
+                ans = cur_n.depth;
+                return true;
+            }
+            if(cur_n.depth + cur_n.md > limit) continue;
+            int s_x = cur_n.space / N;
+            int s_y = cur_n.space % N;
+            for (int i = 0; i < 4; ++i)
+            {
+                Node next_n = cur_n;
+                int new_x = s_x + dx[i];
+                int new_y = s_y + dy[i];
+                if(new_x < 0  || new_y < 0 || new_x >= N || new_y >= N) continue; 
+                if(max(cur_n.pre, i) - min(cur_n.pre, i) == 2) continue;
+     
+                //incremental manhattan distance
+                next_n.md -= md[new_x * N + new_y][next_n.puzzle[new_x * N + new_y] - 1];
+                next_n.md += md[s_x * N + s_y][next_n.puzzle[new_x * N + new_y] - 1];
+     
+                swap(next_n.puzzle[new_x * N + new_y], next_n.puzzle[s_x * N + s_y]);
+                next_n.space = new_x * N + new_y;
+                // assert(get_md_sum(new_n.puzzle) == new_n.md);
+                // return dfs(new_n, depth+1, i);
+                next_n.depth++;
+                if(cur_n.depth + cur_n.md > limit) continue;
+                next_n.pre = i;
+                st.push(next_n);
+                if(next_n.md == 0) {
+                    ans = next_n.depth;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	bool dfs(Node cur_n, int depth, int pre) {
-		if(cur_n.md == 0 ) {
-			ans = depth;
-			return true;
-		}
-		if(depth + cur_n.md > limit) return false;
-		int s_x = cur_n.space / N;
-		int s_y = cur_n.space % N;
-		for (int i = 0; i < 4; ++i)
-		{
-			Node new_n = cur_n;
-			int new_x = s_x + dx[i];
-			int new_y = s_y + dy[i];
-			if(new_x < 0  || new_y < 0 || new_x >= N || new_y >= N) continue; 
-			if(max(pre, i) - min(pre, i) == 2) continue;
-
-			new_n.md -= md[new_x * N + new_y][new_n.puzzle[new_x * N + new_y] - 1];
-			new_n.md += md[s_x * N + s_y][new_n.puzzle[new_x * N + new_y] - 1];
-			swap(new_n.puzzle[new_x * N + new_y], new_n.puzzle[s_x * N + s_y]);
-			new_n.space = new_x * N + new_y;
-			// assert(get_md_sum(new_n.puzzle) == new_n.md);
-			// return dfs(new_n, depth+1, i);
-			if(dfs(new_n, depth + 1, i)){
-				path[depth] = i;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	void ida_star() {
-		for (limit = s_node.md; limit < 1000; ++limit)
-		{
-			path.resize(limit);
-			if(dfs(s_node, 0, -10)) {
-				cout << limit << endl;
-				string str = "";
-				for (int i = 0; i < limit; ++i)
-				{
-					str += dir[path[i]];
-				}
-				cout << str << endl;
-				return;
-			}
-		}
-	}
-
-	void exec() {
-		ida_star();
-	}
+    void ida_star() {
+        for (int limit = s_node.md; limit < 1000; ++limit, ++limit)
+        {
+            // path.resize(limit);
+            cur_n = s_node;
+            if(dfs(limit)) {
+                cout << limit << endl;
+                return;
+            }
+        }
+    }
+ 
+    void exec() {
+        ida_star();
+    }
 };
-
-
+ 
+ 
 int main() {
-	Npuzzle np = Npuzzle();
-	np.exec(); 
+    Npuzzle np = Npuzzle();
+    np.exec(); 
 }
-
