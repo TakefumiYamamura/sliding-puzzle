975c83b20ad62fe2ad26bcb38f2ff7561741463f
TakefumiYamamura <yama1223xxx@gmail.com>
2017-10-02 10:25:33 +0900

    place const and device memory

--- src/24puzzle_psimple_with_pdb.cu
+++ src/24puzzle_psimple_with_pdb.cu
@@ -51,6 +51,12 @@ static const int dy[4] = {1, 0, -1, 0};
 // static const char dir[4] = {'r', 'u', 'l', 'd'}; 
 static const int order[4] = {1, 0, 2, 3};
 
+static __device__ __constant__ const int rf[] = {0,5,10,15,20,1,6,11,16,21,2,7,12,17,22,3,8,13,18,23,4,9,14,19,24};
+static __device__ __constant__ const int rot90[] = {20,15,10,5,0,21,16,11,6,1,22,17,12,7,2,23,18,13,8,3,24,19,14,9,4};
+static __device__ __constant__ const int rot90rf[] = {20,21,22,23,24,15,16,17,18,19,10,11,12,13,14,5,6,7,8,9,0,1,2,3,4};
+static __device__ __constant__ const int rot180[] = {24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+static __device__ __constant__ const int rot180rf[] = {24,19,14,9,4,23,18,13,8,3,22,17,12,7,2,21,16,11,6,1,20,15,10,5,0};
+
 
 struct Node
 {
@@ -125,19 +131,6 @@ private:
     unsigned char h0[PDB_TABLESIZE];
     unsigned char h1[PDB_TABLESIZE];
     /* the position of each tile in order, reflected about the main diagonal */
-    int rf[N2];
-
-    /* rotates the puzzle 90 degrees */
-    int rot90[N2];
-
-    /* composes the reflection and 90 degree rotation into a single array */
-    int rot90rf[N2];
-
-    /* rotates the puzzle 180 degrees */
-    int rot180[N2];
-
-    /* composes the reflection and 180 degree rotation into a single array */
-    int rot180rf[N2];
 public:
     PatternDataBase();
     void input(const char *filename, unsigned char *table);
@@ -155,13 +148,7 @@ public:
 
 };
 
-PatternDataBase::PatternDataBase() : rf{0, 5, 10, 15, 20, 1, 6, 11, 16, 21, 2, 7, 12, 17, 22, 3, 8, 13, 18, 23, 4, 9, 14, 19, 24}, 
-                                    rot90{20, 15, 10, 5, 0, 21, 16, 11, 6, 1, 22, 17, 12, 7, 2, 23, 18, 13, 8, 3, 24, 19, 14, 9, 4},
-                                    rot90rf{20, 21, 22, 23, 24, 15, 16, 17, 18, 19, 10, 11, 12, 13, 14, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4},
-                                    rot180{24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-                                    rot180rf{24, 19, 14, 9, 4, 23, 18, 13, 8, 3, 22, 17, 12, 7, 2, 21, 16, 11, 6, 1, 20, 15, 10, 5, 0}
-
-{
+PatternDataBase::PatternDataBase()　{
     const char *c0 = "../pdb/pat24.1256712.tab";
     const char *c1 = "../pdb/pat24.34891314.tab";
     cout << "pattern 1 2 5 6 7 12 read in" << endl;
@@ -326,12 +313,7 @@ public:
 
 };
 
-local_pdb::local_pdb() : rf{0, 5, 10, 15, 20, 1, 6, 11, 16, 21, 2, 7, 12, 17, 22, 3, 8, 13, 18, 23, 4, 9, 14, 19, 24}, 
-                        rot90{20, 15, 10, 5, 0, 21, 16, 11, 6, 1, 22, 17, 12, 7, 2, 23, 18, 13, 8, 3, 24, 19, 14, 9, 4},
-                        rot90rf{20, 21, 22, 23, 24, 15, 16, 17, 18, 19, 10, 11, 12, 13, 14, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4},
-                        rot180{24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-                        rot180rf{24, 19, 14, 9, 4, 23, 18, 13, 8, 3, 22, 17, 12, 7, 2, 21, 16, 11, 6, 1, 20, 15, 10, 5, 0}
-{
+local_pdb::local_pdb() {
     for (int i = 0; i < PDB_TABLESIZE; ++i)
     {
         h0[i] = pdb.h0[i];
