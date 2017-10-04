bb5eeedadbca3bff4d86e4613c89228ce02b253d
TakefumiYamamura <yama1223xxx@gmail.com>
2017-10-05 07:03:59 +0900

    15 puzzle

--- src/15puzzle_psimple.cu
+++ src/15puzzle_psimple.cu
@@ -355,10 +355,10 @@ int main() {
     // ofstream writing_file;
     // writing_file.open(output_file, std::ios::out);
     FILE *output_file;
-    output_file = fopen("../result/korf100_psimple_result.csv","w");
+    output_file = fopen("../result/korf100_psimple_result_99.csv","w");
 
     set_md();
-    for (int i = 0; i < 50; ++i)
+    for (int i = 0; i < 99; ++i)
     {
         string input_file = "../benchmarks/korf100/prob";
         if(i < 10) {
