c5ad1f5cb9ea2467ce303ebbca40405ea8368138
TakefumiYamamura <yama1223xxx@gmail.com>
2017-11-05 16:13:12 +0900

    convert indentation to tab

--- test/test_in_24puzzle.py
+++ test/test_in_24puzzle.py
@@ -2,38 +2,38 @@ import commands
 import os
 
 def simple_test(exec_file_name):
-    ans_hash = {}
-    for line in open('result_24puzzle.txt', 'r'):
-        array = line.split()
-        if len(array) == 0:
-            break
-        ans_hash[array[0]] = array[1]
+	ans_hash = {}
+	for line in open('result_24puzzle.txt', 'r'):
+		array = line.split()
+		if len(array) == 0:
+			break
+		ans_hash[array[0]] = array[1]
 
 
-    flag = True
-    results = commands.getoutput(exec_file_name)
-    print results
-    outputs = results.split('\n');
-    # print outputs
-    for res in outputs:
-        # print res
-        array = res.split()
-        if len(array) != 2 and len(array) != 3:
-            continue
-        # print res
+	flag = True
+	results = commands.getoutput(exec_file_name)
+	print results
+	outputs = results.split('\n');
+	# print outputs
+	for res in outputs:
+		# print res
+		array = res.split()
+		if len(array) != 2 and len(array) != 3:
+			continue
+		# print res
 
-        if ans_hash[array[0]] != array[1]:
-            flag = False
-            print("answer is different in " + array[0] + " true ans is " + ans_hash[array[0]] + " : false ans is " + array[1])
-        else:
-            # print("answer is same in " + array[0] + " " + ans_hash[array[0]])
+		if ans_hash[array[0]] != array[1]:
+			flag = False
+			print("answer is different in " + array[0] + " true ans is " + ans_hash[array[0]] + " : false ans is " + array[1])
+		else:
+			# print("answer is same in " + array[0] + " " + ans_hash[array[0]])
 
 
-    if flag == True:
-        print(exec_file_name + " this solver is valid")
-    else:
-        print(exec_file_name + " unvalid !!!")
-    print("")
+	if flag == True:
+		print(exec_file_name + " this solver is valid")
+	else:
+		print(exec_file_name + " unvalid !!!")
+	print("")
 
 os.system("g++ -std='c++11' -O3 -o ../src/24puzzle ../src/24puzzle.cc")
 simple_test("../src/./24puzzle")
