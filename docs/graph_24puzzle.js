2ad1579f42262d79cfdf337365cd4ab766281003
TakefumiYamamura <yama1223xxx@gmail.com>
2017-11-16 03:50:47 +0900

    delete log

--- docs/graph_24puzzle.js
+++ docs/graph_24puzzle.js
@@ -48,7 +48,6 @@ window.onload = function () {
   var array2 = loadCSV("../result/yama24_med_result_pdb_wo_cuda.csv");
   var array3 = loadCSV("../result/yama24_med_psimple_result.csv");
   var array4 = loadCSV("../result/yama24_med_psimple_with_pdb_result.csv");
-  console.log(divide_array(array1, array2));
   var chart = new CanvasJS.Chart("chartContainer",
   {
     title:{
