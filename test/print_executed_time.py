046480a015da2a00d0454f054624e31ddc89c232
TakefumiYamamura <yama1223xxx@gmail.com>
2017-12-08 16:27:08 +0900

    modify print executed time

--- test/print_executed_time.py
+++ test/print_executed_time.py
@@ -1,22 +1,44 @@
 import commands
 import os
 
-
 os.system("g++ -std='c++11' -O3 -o calculate_executed_sum calculate_executed_sum.cpp")
 
 print("cpu in 15 puzzle")
-os.system("./calculate_executed_sum ../result/korf100_result_speed.csv 50")
+os.system("./calculate_executed_sum ../result/korf100_result_speed_100.csv 100")
+# print("psimple gpu in 15 puzzle")
+# os.system("./calculate_executed_sum ../result/korf100_psimple_result_50.csv 50")
+# print("psimple shared gpu in 15 puzzle")
+# os.system("./calculate_executed_sum ../result/korf100_psimple_result_50_shared.csv 50")
 print("psimple gpu in 15 puzzle")
-os.system("./calculate_executed_sum ../result/korf100_psimple_result_50.csv 50")
-print("\n")
+os.system("./calculate_executed_sum ../result/korf100_psimple_result_99.csv 99")
+
+print("block parallel gpu in 15 puzzle")
+os.system("./calculate_executed_sum ../result/korf100_block_parallel_result_with_staticlb_100.csv 100")
+print("block parallel gpu in 15 puzzle")
+os.system("./calculate_executed_sum ../result/korf100_block_parallel_result_with_staticlb_100_2048.csv 100")
+
+
+print("------------------------------------")
+
 
 print("cpu in 24 puzzle")
 os.system("./calculate_executed_sum ../result/yama24_med_result.csv 50")
+print("cpu expand in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_expand_result.csv 50")
 print("psimple gpu in 24 puzzle")
 os.system("./calculate_executed_sum ../result/yama24_med_psimple_result.csv 50")
-print("\n")
+print("block parallel gpu in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_block_parallel_result_with_staticlb_100_2048.csv 50")
+print("block parallel gpu dfs in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_block_parallel_result_with_staticlb_dfs_100_2048.csv 50")
 
 print("cpu with pdb in 24 puzzle")
 os.system("./calculate_executed_sum ../result/yama24_med_result_pdb_wo_cuda.csv 50")
+print("cpu expand with pdb in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_result_pdb_expand.csv 50")
 print("psimple with pdb gpu in 24 puzzle")
 os.system("./calculate_executed_sum ../result/yama24_med_psimple_with_pdb_result.csv 50")
+print("block parallel with pdb gpu in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_block_parallel_result_with_pdb_2048.csv 50")
+print("block parallel with pdb gpu dfs in 24 puzzle")
+os.system("./calculate_executed_sum ../result/yama24_med_block_parallel_result_with_pdb_2048_dfs.csv 50")
