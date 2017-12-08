2edf1f2adde5c725f80abc5f60274c29c7d8adbd
TakefumiYamamura <yama1223xxx@gmail.com>
2017-12-08 16:36:03 +0900

    modify output

--- test/print_executed_time.py
+++ test/print_executed_time.py
@@ -5,15 +5,14 @@ os.system("g++ -std='c++11' -O3 -o calculate_executed_sum calculate_executed_sum
 
 print("cpu in 15 puzzle")
 os.system("./calculate_executed_sum ../result/korf100_result_speed_100.csv 100")
-# print("psimple gpu in 15 puzzle")
-# os.system("./calculate_executed_sum ../result/korf100_psimple_result_50.csv 50")
-# print("psimple shared gpu in 15 puzzle")
-# os.system("./calculate_executed_sum ../result/korf100_psimple_result_50_shared.csv 50")
+print("cpu expand in 15 puzzle")
+os.system("./calculate_executed_sum ../result/korf100_result_expand_100.csv 100")
 print("psimple gpu in 15 puzzle")
-os.system("./calculate_executed_sum ../result/korf100_psimple_result_99.csv 99")
-
+os.system("./calculate_executed_sum ../result/korf100_psimple_result_50.csv 50")
+print("psimple shared gpu in 15 puzzle")
+os.system("./calculate_executed_sum ../result/korf100_psimple_result_50_shared.csv 50")
 print("block parallel gpu in 15 puzzle")
-os.system("./calculate_executed_sum ../result/korf100_block_parallel_result_with_staticlb_100.csv 100")
+os.system("./calculate_executed_sum ../result/korf100_block_parallel_result_with_staticlb_dfs_100_2048.csv 100")
 print("block parallel gpu in 15 puzzle")
 os.system("./calculate_executed_sum ../result/korf100_block_parallel_result_with_staticlb_100_2048.csv 100")
 
